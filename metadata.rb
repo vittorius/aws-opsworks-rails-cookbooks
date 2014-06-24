7c2a03751c2df1b783d6e52ad848262cedc9df8e
vittorius <post.vittorius@gmail.com>
2014-06-24 13:51:03 +0300

    Create a root metadata file for OpsWorks Berkshelf

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name        'aws-opsworks-rails-cookbooks'
+description 'Additional recipes to deploy Rails apps to OpsWorks'
+maintainer  'vittorius'
+license     'MIT'
+version     '1.0.0'
+
+recipe 'rails::assets-precompile', 'Precompile assets'
