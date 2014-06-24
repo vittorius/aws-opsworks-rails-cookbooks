d2805cbec281bfb54d27bff6c5ad6958f635fee7
vittorius <post.vittorius@gmail.com>
2014-06-24 14:06:30 +0300

    Remove Berkshelf file and metadata

--- metadata.rb
+++
@@ -1,7 +0,0 @@
-name        'aws-opsworks-rails-cookbooks'
-description 'Additional recipes to deploy Rails apps to OpsWorks'
-maintainer  'vittorius'
-license     'MIT'
-version     '1.0.0'
-
-recipe 'rails::assets-precompile', 'Precompile assets'
--- rails/metadata.rb
+++ rails/metadata.rb
@@ -1,7 +1,7 @@
-name        'deploy'
+name        'rails'
 description 'Additional recipes to deploy Rails apps to OpsWorks'
 maintainer  'vittorius'
 license     'MIT'
 version     '1.0.0'
 
-recipe 'deploy::assets-precompile', 'Precompile assets'
+recipe 'rails::assets-precompile', 'Precompile assets'
